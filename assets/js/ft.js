193a97c0c730fa9950aded06e2860a7f447a28ab
迷妹 <ursxie@gmail.com>
2022-03-26 14:03:45 +0800

    up

---
+++ assets/js/ft.js
@@ -0,0 +1,22 @@
+function a(){
+	var div=document.getElementById("d1");
+	div.style.display="block"
+	}
+	window.onload=function(){
+	//1000毫秒后弹出根据自己的需要设置时间
+	setTimeout("a()",1000);
+	}
+
+$(document).ready(function(){
+    $('#agree').click(function(){
+        if($("input[type='checkbox']").is(':checked')){
+           	$("input.btn").click(function() {
+           		
+           	} )
+        }else{
+
+        }
+    });
+});
+
+
