ed2f0eece8467640ab5bd54c4e330e37b4a13f00
迷妹 <ursxie@gmail.com>
2022-03-26 14:20:39 +0800

    Update ft.js

--- assets/js/ft.js
+++ assets/js/ft.js
@@ -3,8 +3,7 @@ function a(){
 	div.style.display="block"
 	}
 	window.onload=function(){
-	//1000毫秒后弹出根据自己的需要设置时间
-	setTimeout("a()",1000);
+	setTimeout("a()",5000);
 	}
 
 $(document).ready(function(){
